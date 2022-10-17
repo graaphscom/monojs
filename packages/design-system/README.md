3f2df27b61c6f913b3c7459a5ff2adeb1ceb1a45
Konrad Mikucki <komik966@gmail.com>
2022-10-18 00:17:55 +0200

    init

---
+++ packages/design-system/README.md
@@ -0,0 +1,9 @@
+# Graaphs Design System
+
+## Inspired by
+In alphabetical order:
+1. [Carbon Design System](https://carbondesignsystem.com)
+2. [Compass Design System](https://zeroheight.com/29be2c109/p/995619-compass-design-system)
+3. [Fluent Design System](https://www.microsoft.com/design/fluent)
+4. [Human Interface Guidelines](https://developer.apple.com/design/human-interface-guidelines)
+5. [Material Design](https://material.io)
---
+++ packages/design-system/foundations/color.ts
@@ -0,0 +1,96 @@
+export const primary = 'rgb(0, 0, 255)' // "blue"
+export const secondary = 'rgb(65, 105, 225)' // "royalblue"
+export const tertiary = 'rgb(123, 104, 238)' // "mediumslateblue"
+export const quaternary = 'rgb(100, 149, 237)' // "cornflowerblue"
+
+export const light = {
+    red: 'rgb(255, 59, 48)',
+    orange: 'rgb(255, 149, 0)',
+    yellow: 'rgb(255, 204, 0)',
+    green: 'rgb(52, 199, 89)',
+    mint: 'rgb(0, 199, 190)',
+    teal: 'rgb(48, 176, 199)',
+    cyan: 'rgb(50, 173, 230)',
+    blue: 'rgb(0, 122, 255)',
+    indigo: 'rgb(88, 86, 214)',
+    purple: 'rgb(175, 82, 222)',
+    pink: 'rgb(255, 45, 85)',
+    brown: 'rgb(162, 132, 94)',
+    gray: {
+        1: 'rgb(142, 142, 147)',
+        2: 'rgb(174, 174, 178)',
+        3: 'rgb(199, 199, 204)',
+        4: 'rgb(209, 209, 214)',
+        5: 'rgb(229, 229, 234)',
+        6: 'rgb(242, 242, 247)'
+    }
+}
+export const lightAccessible = {
+    red: 'rgb(215, 0, 21)',
+    orange: 'rgb(201, 52, 0)',
+    yellow: 'rgb(178, 80, 0)',
+    green: 'rgb(36, 138, 61)',
+    mint: 'rgb(12, 129, 123)',
+    teal: 'rgb(0, 130, 153)',
+    cyan: 'rgb(0, 113, 164)',
+    blue: 'rgb(0, 64, 221)',
+    indigo: 'rgb(54, 52, 163)',
+    purple: 'rgb(137, 68, 171)',
+    pink: 'rgb(211, 15, 69)',
+    brown: 'rgb(127, 101, 69)',
+    gray: {
+        1: 'rgb(108, 108, 112)',
+        2: 'rgb(142, 142, 147)',
+        3: 'rgb(174, 174, 178)',
+        4: 'rgb(188, 188, 192)',
+        5: 'rgb(216, 216, 220)',
+        6: 'rgb(235, 235, 240)'
+    }
+}
+
+
+export const dark = {
+    red: 'rgb(255, 69, 58)',
+    orange: 'rgb(255, 159, 10)',
+    yellow: 'rgb(255, 214, 10)',
+    green: 'rgb(48, 209, 88)',
+    mint: 'rgb(102, 212, 207)',
+    teal: 'rgb(64, 200, 224)',
+    cyan: 'rgb(100, 210, 255)',
+    blue: 'rgb(10, 132, 255)',
+    indigo: 'rgb(94, 92, 230)',
+    purple: 'rgb(191, 90, 242)',
+    pink: 'rgb(255, 55, 95)',
+    brown: 'rgb(172, 142, 104)',
+    gray: {
+        1: 'rgb(142, 142, 147)',
+        2: 'rgb(99, 99, 102)',
+        3: 'rgb(72, 72, 74)',
+        4: 'rgb(58, 58, 60)',
+        5: 'rgb(44, 44, 46)',
+        6: 'rgb(28, 28, 30)'
+    }
+}
+
+export const darkAccessible = {
+    red: 'rgb(255, 105, 97)',
+    orange: 'rgb(255, 179, 64)',
+    yellow: 'rgb(255, 212, 38)',
+    green: 'rgb(48, 219, 91)',
+    mint: 'rgb(102, 212, 207)',
+    teal: 'rgb(93, 230, 255)',
+    cyan: 'rgb(112, 215, 255)',
+    blue: 'rgb(64, 156, 255)',
+    indigo: 'rgb(125, 122, 255)',
+    purple: 'rgb(218, 143, 255)',
+    pink: 'rgb(255, 100, 130)',
+    brown: 'rgb(181, 148, 105)',
+    gray: {
+        1: 'rgb(174, 174, 178)',
+        2: 'rgb(124, 124, 128)',
+        3: 'rgb(84, 84, 86)',
+        4: 'rgb(68, 68, 70)',
+        5: 'rgb(54, 54, 56)',
+        6: 'rgb(36, 35, 38)'
+    }
+}
